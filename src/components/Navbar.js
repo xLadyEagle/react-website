01fce0bbc27109135bf9a26a23cce9738223b575
xLadyEagle <82974182+xLadyEagle@users.noreply.github.com>
2021-10-22 17:11:08 +0200

    Deleted button

---
+++ src/components/Navbar.js
@@ -0,0 +1,75 @@
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Fragment } from 'react/cjs/react.production.min';
+import './Navbar.css';
+
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+    const [button, setButton] = useState(true);
+
+    const handleClick = () => {
+        setClick(!click);
+    };
+
+    const closeMobileMenu = () => {
+        setClick(false);
+    };
+
+   
+    return (
+        <Fragment>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        JAPAN <i className="fas fa-spa"></i>
+                    </Link>
+                    <div className="menu-icon" onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className="nav-item">
+                            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
+                                Home
+                            </Link>
+                        </li>
+                        <li className="nav-item dropdown">
+                            <Link to= "" className="dropbtn">Cosa vedere</Link>
+                            <div className="dropdown-content">
+                                <Link to="">Kyoto</Link>
+                                <Link to="">Tokyo</Link>
+                                <Link to="">Nara</Link>	
+                                <Link to="">Osaka</Link>	
+                            </div>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/primavera" className="nav-links" onClick={closeMobileMenu}>
+                                Primavera in Giappone
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/cucina" className="nav-links" onClick={closeMobileMenu}>
+                                Cucina giapponese
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/curiosità" className="nav-links" onClick={closeMobileMenu}>
+                                Curiosità
+                            </Link>
+                        </li>
+                        <li className='nav-item'>
+                            <Link to="/sign-up" className="nav-links-mobile" onClick={closeMobileMenu}>
+                                Sign Up
+                            </Link>
+                        </li>
+                    </ul>
+    
+                </div>
+            </nav>
+
+
+        </Fragment>
+    )
+}
+
+export default Navbar;
